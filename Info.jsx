a18b9d9d31056c094413a8baa60417d6cb18caa8
deep patel <81354794+PatelDeep7900@users.noreply.github.com>
2021-06-27 20:04:39 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and Node.js</h1>
+      <p>free 7 days live BOOTCAMP with a hands on project at the end</p>
+      <p>Date: 27-06-2021</p>
+      <p>Time: 6:00 pm</p>
+    </div>
+  );
+}
+
+export default Info;
